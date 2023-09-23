f7d837767a26a4b21d93bb60aa3df8d1f0abb5bb
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2023-09-23 10:41:34 -0300

    syntax db | filtrados

--- tpi.pl
+++ tpi.pl
@@ -23,39 +23,68 @@ menu:-
 menu.
 
 
-opcion(1):- write("Opciones de filtrado"),nl,
-            write("1 - Filtrar por liga"),nl,
-            write("2 - Filtrar por pais"),nl,
-            read(Opcion),
-            filtrar(Opcion).
+opcion(1):- 
+    write("Opciones de filtrado"),nl,
+    write("1 - Filtrar por liga"),nl,
+    write("2 - Filtrar por pais"),nl,   %TODO --> opcion dentro "desea conocer el horario de los partidos?" y que muestre los estadios
+    read(Opcion),
+    filtrar(Opcion).
 
-  mostrarLigas():- liga_es_de(Liga,_), write(Liga),nl,fail.
+    mostrarLigas:-
+        write("Las ligas cargadas en el sistema son: "),nl,
+        findall(Liga, liga_es_de(Liga,_), Ligas),
+        mostrar(Ligas).
+    
+    mostrar([]).
+    mostrar([X|Xs]):-
+        write(X),nl,
+        mostrar(Xs).
 
-  filtrar(1):- write("Las ligas cargadas en el sistema son: "),nl,
-              mostrarLigas(),
-              write("Ingrese el nombre de la liga: "),
-              read(Liga).
+    filtrar(1):- write("Las ligas cargadas en el sistema son: "),nl,
+                mostrarLigas(),
+                write("Ingrese el nombre de la liga: "),
+                read(Liga),
+                write("Los partidos de la liga "), write(Liga), write(" son: "), nl,
+                obtenerPartidosLiga(Liga),
+                menu.
 
-  mostrarPaises():- equipo_es_de(_,Pais), write(Pais),nl,fail.
+    obtenerPartidosLiga(Liga):- 
+        partidos_hoy(Equipo1,Equipo2,_,_),
+        equipo_es_de(Equipo1,Pais),
+        equipo_es_de(Equipo2,Pais),
+        liga_es_de(Liga,Pais),
+        write(Equipo1),write(" vs "),write(Equipo2), nl, fail.
 
-  filtrar(2):- write("Los paises cargados en el sistema son: "),nl,
-              mostrarPaises(),
-              write("Ingrese el nombre del pais: "),
-              read(Pais).
-        
 
-opcion(2):- write("Los paises cargados en el sistema son: "),nl,
-            mostrarPaises(),
-            write("Ingrese el nombre del pais: "),
-            read(Pais),
-            write("Las ligas pertenecientes a ese pais son: "),nl,
-            liga_es_de(Liga,Pais),
-            write(Liga),nl,
-            write("Ingrese el nombre de la liga: "),
-            read(Liga),
-            write("Los ultimos campeones de la liga "),write(Liga),write(" son: "),nl,
-            campeon_liga(_,Liga,Anio),
-            write(Anio),nl.
+    mostrarPaises():- equipo_es_de(_,Pais), write(Pais),nl,fail.
+
+    filtrar(2):- write("Los paises cargados en el sistema son: "),nl,
+                mostrarPaises(),
+                write("Ingrese el nombre del pais: "),
+                read(Pais),
+                write("Los partidos del pais "), write(Pais), write(" son: "), nl,
+                obtenerPartidosPais(Pais),
+                menu.
+
+    obtenerPartidosPais(Pais):-
+        partidos_hoy(Equipo1,Equipo2,_,_),
+        equipo_es_de(Equipo1,Pais),
+        equipo_es_de(Equipo2,Pais),
+        write(Equipo1),write(" vs "),write(Equipo2), nl, fail.
+    
+opcion(2):- 
+    write("Los paises cargados en el sistema son: "),nl,
+    mostrarPaises(),
+    write("Ingrese el nombre del pais: "),
+    read(Pais),
+    write("Las ligas pertenecientes a ese pais son: "),nl,
+    liga_es_de(Liga,Pais),
+    write(Liga),nl,
+    write("Ingrese el nombre de la liga: "),
+    read(Liga),
+    write("Los ultimos campeones de la liga "),write(Liga),write(" son: "),nl,
+    campeon_liga(_,Liga,Anio),
+    write(Anio),nl.
 
 opcion(3):- write("Opcion 3").
             
