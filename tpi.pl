e1e2239f2932c6ed803a2ba3a73e8ec8471c4ea1
Luchoragusa <luchito.rc4@gmail.com>
2023-09-28 11:21:18 -0300

    Version 2

--- tpi.pl
+++ tpi.pl
@@ -1,4 +1,4 @@
-:- dynamic(partidos_hoy/4).
+:- dynamic(partidos_hoy/5).
 :- dynamic(equipo_es_de/2).
 :- dynamic(campeon_liga/3).
 :- discontiguous filtrar/1.
@@ -17,167 +17,138 @@ guardar_base:-
     listing(campeon_liga),
     told.
 
-menu:-
-    abrir_base, nl,
-    write("======= Menu ======="),nl,
-    write("Opcion 1 - Mostrar los partidos del dia de hoy en base a una liga/pais"),nl,
-    write("Opcion 2 - Mostrar los ultimos campeones de una determinada liga de un pais"),nl,
-    write("Opcion 0 - Salir"),
-    nl,
+inicio:- 
+    write("Bienvenido al sistema de partidos de futbol! Ingrese su nombre"),nl,
+    read(Nombre),
+    nl,write("Hola "),write(Nombre),write("! Te interesa ver partidos hoy?"),nl,
     read(Opcion),
-    nl,
-    Opcion\=0,
-    opcion(Opcion),
-    menu.
-menu.
-
-
-menu_si_no(Opcion):-
-    nl,
-    write("1 - Si"),nl,
-    write("2 - No"),nl,
-    read(Opcion),
-    validar_opcion(Opcion).
-validar_opcion(1).
-validar_opcion(2).
-validar_opcion(_) :-
-    write("Opcion no valida. Por favor, seleccione 1 o 2."), nl,
-    menu_si_no(_).
-
-
-opcion(1):-
-    write("Opciones de filtrado"),nl,
-    write("1 - Filtrar por liga"),nl,
-    write("2 - Filtrar por pais"),nl,
-    read(Opcion),
-    nl,
-    filtrar(Opcion).
-
-    filtrar(1):-write("Las ligas cargadas en el sistema son: "),nl,
-                abrir_base,
-                mostrarLigas([]),
-                write("Ingrese el nombre de la liga: "),
-                read(Liga),nl,
-                abrir_base, %% Usamos el abrir_base pq nosotros arriba usamos el retract para eliminar las ligas q fuimos mostrando
-                verificar_liga(Liga), %% Verificamos si la liga existe, si falla, vuelve al write de "Ingrese el nombre de la liga: " | Linea 59
-                write("Quiere conecer ademas el estadio y el horario en el que se juega?"),nl,
-                menu_si_no(Opcion), nl,
-                write("Los partidos de la liga "), write(Liga), write(" son: "), nl,
-                obtenerPartidosLiga(Liga, Opcion).
-        
-        mostrarLigas(LigasMostrados):- %% ligasMostradas es una lista que contiene las ligas que ya han sido mostradas
-            equipo_es_de(_, liga_es_de(Liga, _)),
-            \+ member(Liga, LigasMostrados), %% Verificar si la liga esta o no en la cadena LigasMostradas
-            write("= "), write(Liga),nl,
-            retract(equipo_es_de(_, liga_es_de(Liga, _))),
-            mostrarLigas([Liga|LigasMostrados]). %% Agregar la liga a la cadena de LigasMostradas, como un push
-        mostrarLigas(_):- nl,nl.
-            
-        verificar_liga(Liga):- equipo_es_de(_,liga_es_de(Liga, _)).  
-        verificar_liga(_):- write("La Liga ingresada no existe"),nl, fail.
-
-        obtenerPartidosLiga(Liga, Opcion):-
-            equipo_es_de(Equipo1, liga_es_de(Liga, Pais)),
-            equipo_es_de(Equipo2, liga_es_de(Liga, Pais)),
-            Equipo1 \= Equipo2,
-            partidos_hoy(Equipo1,Equipo2,Hora,Estadio), %% Traemos el horario y el estadio, para cuando hacemos el retract, no eliminar ese partido de antes.
-            write("=> "), write(Equipo1),write(" vs "),write(Equipo2), nl,
-            retract(partidos_hoy(Equipo1,Equipo2,Hora,Estadio)),
-            mostrar_hora_estadio(Hora, Estadio, Opcion),
-            obtenerPartidosLiga(Liga, Opcion).
-        obtenerPartidosLiga(_, _):- nl.
-
-            mostrar_hora_estadio(Hora, Estadio, 1):-
-                write("= "), write("El horario del partido es: "), write(Hora), nl,
-                write("= "), write("El estadio del partido es: "), write(Estadio), nl.
-
-
-    filtrar(2):- write("Los paises cargados en el sistema son: "),nl,
-                mostrarPaises([]),
-                abrir_base,
-                write("Ingrese el nombre del pais: "),
-                read(Pais),
-                verificar_pais(Pais),
-                write("Quiere conecer ademas el estadio y el horario en el que se juega?"),nl,
-                menu_si_no(Opcion),
-                write("Los partidos del pais "), write(Pais), write(" son: "), nl,
-                obtenerPartidosPais(Pais, Opcion).
-
-        mostrarPaises(PaisesMostrados):-
-            equipo_es_de(_,liga_es_de(_, Pais)),
-            \+ member(Pais, PaisesMostrados), % Verificar si el país ya ha sido mostrado
-            write("= "), write(Pais),nl,
-            retract(equipo_es_de(_,liga_es_de(_, Pais))),
-            mostrarPaises([Pais|PaisesMostrados]).
-        mostrarPaises(_):- nl.
-    
-        verificar_pais(Pais):- equipo_es_de(_,liga_es_de(_,Pais)).
-        verificar_pais(_):- nl,write("El Pais ingresado no existe"),nl,fail.
-
-        obtenerPartidosPais(Pais, Opcion):-
-            equipo_es_de(Equipo1,liga_es_de(Liga, Pais)),
-            equipo_es_de(Equipo2,liga_es_de(Liga, Pais)),
-            Equipo1 \= Equipo2,
-            partidos_hoy(Equipo1,Equipo2,Hora,Estadio),
-            write("=> "), write(Equipo1),write(" vs "),write(Equipo2), nl,
-            retract(partidos_hoy(Equipo1,Equipo2,Hora,Estadio)),
-            mostrar_hora_estadio(Hora, Estadio, Opcion),    %es el de arriba
-            obtenerPartidosPais(Pais, Opcion).
-        obtenerPartidosPais(_,_):- nl.
-
-    filtrar(_):- % condicion de comprobacion 
-        write("Opcion no valida. Por favor, seleccione 1 o 2."),
-        nl,nl,
-        opcion(1).
-
-opcion(2):-
-    write("Los paises cargados en el sistema son: "),nl,
-    mostrarPaises([]),
-    abrir_base,
-    write("Ingrese el nombre del pais: "),
+    Opcion\='no',
+    Opcion\='No',
+    validar_opcion(Opcion),
+    nl,write("Las ligas de que pais te interesan?"),nl,
     read(Pais),
-    verificar_pais(Pais),
-    write("Las ligas pertenecientes a ese pais son: "),nl,
-    mostrar_ligas_pais(Pais, []),
-    write("Ingrese el nombre de la liga: "),
+    abrir_base,
+    validar_pais(Pais),
+    nl,write("Conoces el nombre de las ligas del pais "), write(Pais), write("?"),nl,
+    read(OpcionLigas),
+    abrir_base,
+    validar_opcion_ligas(OpcionLigas,Pais),
+    nl,write("Que liga te interesa ver?"),nl,
     read(Liga),
     abrir_base,
-    verificar_liga_pais(Liga,Pais),
+    validar_liga_pais(Liga,Pais),
+    nl,write("Que servicio de streaming tenes contratado?"),nl,
+    read(Streaming),
+    abrir_base,
+    validar_streaming(Streaming),
+    nl,write("Quiere conecer ademas el estadio y el horario en el que se juega?"),nl,
+    read(Respuesta),
+    validar_horaestadiosino(Respuesta,HoraEstadioSiNo),
+    mostar_partidos(Streaming,Liga,HoraEstadioSiNo),
+    nl,write("Te interesa ver los ultimos campeones de esa Liga?"),nl,
+    read(OpcionCampeon),
+    validar_opcioncampeon(OpcionCampeon),
+    nl,write("Gracias por usar el sistema!"),nl,
+    menu.
+inicio. 
+
+validar_opcion('si').
+validar_opcion('Si').
+validar_opcion(_) :-
+    write("No pude entenderte, Te interesa ver partidos hoy? (si/no)"),
+    nl.
+
+validar_pais(Pais):- equipo_es_de(_,liga_es_de(_,Pais)).
+validar_pais(_):- nl,write("El Pais ingresado no existe"),nl,fail.
+
+validar_opcion_ligas(OpcionLigas):- OpcionLigas='si',mostrar_ligas_pais(Pais, []).
+validar_opcion_ligas(OpcionLigas):- OpcionLigas='Si',mostrar_ligas_pais(Pais, []).
+validar_opcion_ligas(OpcionLigas):- OpcionLigas='no',fail.
+validar_opcion_ligas(OpcionLigas):- OpcionLigas='No',fail.
+validar_opcion_ligas(_,Pais):- 
+    nl,write("No pude entenderte, Conoces el nombre de las ligas del pais "),write(Pais), write("? (si/no)"),nl,
+    read(OpcionLigas),
+    validar_opcion_ligas(OpcionLigas,Pais).
+
+mostrar_ligas_pais(Pais, LigasMostradas):- 
+    equipo_es_de(_,liga_es_de(Liga, Pais)),
+    \+ member(Liga, LigasMostradas), % Verificar si el país ya ha sido mostrado
+    write("-> "), write(Liga),nl,
+    retract(equipo_es_de(_,liga_es_de(Liga, Pais))),
+    mostrar_ligas_pais(Pais, [Liga|LigasMostradas]).
+mostrar_ligas_pais(_, _).
+
+validar_liga_pais(Liga,Pais):- equipo_es_de(_,liga_es_de(Liga,Pais)).
+validar_liga_pais(_,_):- write("La Liga ingresada no existe en ese Pais"),nl,fail.
+
+validar_streaming(Streaming):- partidos_hoy(Streaming,_,_,_,_).
+validar_streaming(Streaming):- nl,write("No hay partidos hoy para esa Liga transmitidos por ese servicio de Streaming"),nl.
+
+validar_horaestadiosino('si', 1).
+validar_horaestadiosino('Si', 1).
+validar_horaestadiosino('no', 2).
+validar_horaestadiosino('No', 2).
+validar_horaestadiosino(_, _):- 
+    nl,write("No pude entenderte, Quiere conocer ademas el estadio y el horario en el que se juega? (si/no)"),nl,
+    read(HoraEstadioSiNo),
+    validar_horaestadiosino(HoraEstadioSiNo, _).
+
+mostrar_partidos(Streaming,Liga,HoraEstadioSiNo):- 
+    equipo_es_de(Equipo1, liga_es_de(Liga, Pais)),
+    equipo_es_de(Equipo2, liga_es_de(Liga, Pais)),
+    Equipo1 \= Equipo2,
+    partidos_hoy(Streaming,Equipo1,Equipo2,Hora,Estadio), %% Traemos el horario y el estadio, para cuando hacemos el retract, no eliminar ese partido de antes.
+    write("=> "), write(Equipo1),write(" vs "),write(Equipo2), nl,
+    retract(partidos_hoy(Streaming,Equipo1,Equipo2,Hora,Estadio)),
+    mostrar_hora_estadio(Hora, Estadio, HoraEstadioSiNo),
+    mostrar_partidos(Liga, Opcion).
+mostrar_partidos(_,_):- nl.
+
+mostrar_hora_estadio(Hora, Estadio, 1):-
+    write("= "), write("El horario del partido es: "), write(Hora), nl,
+    write("= "), write("El estadio del partido es: "), write(Estadio), nl.
+
+validar_opcioncampeon(_,'Si'):- campeones(Liga).
+validar_opcioncampeon(_,'si'):- campeones(Liga).
+validar_opcioncampeon(_,'no').
+validar_opcioncampeon(_,'No').
+validar_opcioncampeon(_, _):- 
+    nl,write("No pude entenderte, Te interesa ver los ultimos campeones de esa Liga? (si/no)"),nl,
+    read(OpcionCampeon),
+    validar_opcioncampeon(_, OpcionCampeon).
+
+campeones(Liga):-
     write("Los ultimos campeones de la liga "),write(Liga),write(" son: "), nl,nl,
+    abrir_base,
     mostrar_campeon_liga(Liga),
-    write("Desea agregar algun otro equipo campeon para esta liga ?"),
-    menu_si_no(Opcion),
-    agregar_campeon(Opcion, Liga).
-    
-        mostrar_ligas_pais(Pais, LigasMostradas):- % por si un pais tiene +1 liga
-            equipo_es_de(_,liga_es_de(Liga, Pais)),
-            \+ member(Liga, LigasMostradas), % Verificar si el país ya ha sido mostrado
-            write("= "), write(Liga),nl,
-            retract(equipo_es_de(_,liga_es_de(Liga, Pais))),
-            mostrar_ligas_pais(Pais, [Liga|LigasMostradas]).
-        mostrar_ligas_pais(_, _).
+    nl,write("Desea agregar algun otro equipo campeon para esta liga ?"),
+    read(Respuesta),
+    validar_agregarcampeon(Respuesta).
 
-        verificar_liga_pais(Liga,Pais):- equipo_es_de(_,liga_es_de(Liga,Pais)).
-        verificar_liga_pais(_,_):- write("La Liga ingresada no existe en ese Pais"),nl,fail.
+mostrar_campeon_liga(Liga):-
+    campeon_liga(Equipo, Liga, Anio),
+    write("= "), write(Equipo), write(" - "), write(Anio),nl,
+    retract(campeon_liga(Equipo, Liga, Anio)),
+    mostrar_campeon_liga(Liga).
+mostrar_campeon_liga(_):-  nl.
 
-        mostrar_campeon_liga(Liga):-
-            campeon_liga(Equipo, Liga, Anio),
-            write("= "), write(Equipo), write(" - "), write(Anio),nl,
-            retract(campeon_liga(Equipo, Liga, Anio)),
-            mostrar_campeon_liga(Liga).
-        mostrar_campeon_liga(_):-  nl.
+validar_agregarcampeon('Si'):- abrir_base,agregar_campeon(Liga).
+validar_agregarcampeon('si'):- abrir_base,agregar_campeon(Liga).
+validar_agregarcampeon('no').
+validar_agregarcampeon('No').
+validar_agregarcampeon(_):-
+    nl,write("No pude entenderte, Desea agregar algun otro equipo campeon para esta liga ? (si/no)"),nl,
+    read(Respuesta),
+    validar_agregarcampeon(Respuesta).
 
-        agregar_campeon(1, Liga):-
-            abrir_base,
-            write("Ingrese el nombre del equipo: "),
-            read(Equipo),
-            write("Ingrese el anio en el que salio campeon: "),
-            read(Anio),
-            assert(campeon_liga(Equipo, Liga, Anio)),
-            guardar_base,
-            write("Desea agregar algun otro equipo campeon para esta liga ?"),
-            menu_si_no(Opcion),
-            agregar_campeon(Opcion, Liga).
-        agregar_campeon(2, _):- write("Gracias por responder!"),nl.
-
-opcion(_):-write("Opcion no valida. Por favor, seleccione una opcion valida."), nl.
+agregar_campeon(Liga):-
+    abrir_base,
+    write("Cual es el nombre del equipo?"),
+    read(Equipo),
+    write("Ingrese el anio en el que salio campeon: "),
+    read(Anio),
+    assert(campeon_liga(Equipo, Liga, Anio)),
+    guardar_base,
+    write("Desea agregar otro equipo campeon para esta liga?"),
+    read(Respuesta),
+    validar_agregarcampeon(Respuesta).
